2e4d7153f13979caf164da008505450a2569ed16
arielpagani <arielpagani@outlook.com>
2022-04-05 20:58:52 -0300

    Heroku

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log('Servidor funcionando');
 });
 
@@ -13,4 +13,8 @@ app.get('/', (req,res)=>{
 
 app.get('/register', (req,res)=>{
     res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
 });
